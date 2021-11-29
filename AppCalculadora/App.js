29b46473ead35bbc7f6ec12ef9cd02481864dd88
John <jhow.mito11@gmail.com>
2021-11-29 16:16:11 -0300

    commit estudos

---
+++ AppCalculadora/App.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+import { View, SafeAreaView, StyleSheet, Text,TextInput, Button} from 'react-native';
+
+export default function() {
+ 
+  const Hello = (props) => {
+    const [name,setName] = useState ('John');
+    const [mostrar, setMostrar] = useState (false);
+
+    const handleClick =() => {
+      setMostrar(!mostrar);
+    }
+ 
+
+    return(
+          <View>
+          <TextInput style={{
+          width:200,
+          height:40,
+          borderWidth: 2,}} 
+          value = {name} onChangeText = {t=>setName(t)}
+          />
+          
+          <Button
+          title = 'Trocar nome'
+          onPress= {handleClick}
+          
+          />
+
+
+          {mostrar &&
+          <View style={{
+            width: 200,
+            height: 200,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderWidth: 2 ,
+            marginTop: 30,
+ 
+          }}>
+            <Text>O nome do jacalé é: </Text>
+            <Text>{name}</Text>
+          </View>
+          }
+         
+          </View>
+    )
+  }
+ 
+  return (
+<SafeAreaView style={{
+                                flex:1,
+                                justifyContent: 'center',
+                                alignItems:'center'
+
+                            }}
+>
+
+<Hello/>
+
+</SafeAreaView>
+  );
+}
